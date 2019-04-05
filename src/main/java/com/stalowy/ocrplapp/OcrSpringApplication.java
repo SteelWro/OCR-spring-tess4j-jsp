cc9434b8a8d9994e93893b2111a2d53c2452a66d
SteelWro <cwiklinski.artur@wp.pl>
2019-04-05 13:55:38 +0200

    Initial commit

---
+++ src/main/java/com/stalowy/ocrplapp/OcrSpringApplication.java
@@ -0,0 +1,13 @@
+package com.stalowy.ocrplapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrSpringApplication.class, args);
+    }
+
+}
