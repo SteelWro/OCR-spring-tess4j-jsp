607e5ef1b52f07d778fb4f606321c602a0ceadbd
SteelWro <cwiklinski.artur@wp.pl>
2019-05-31 15:18:47 +0200

    hotFix: change annotation name

--- src/main/java/com/stalowy/ocrplapp/controller/OcrController.java
+++ src/main/java/com/stalowy/ocrplapp/controller/OcrController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +30,7 @@ public class OcrController {
         this.ocrFileService = ocrFileService;
     }
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String homeSite(Model model) {
         urlResult = new UrlResult();
         fileResult = new FileResult();
