58ac6c5b53f0d4a480604a332a663e66aa703bde
stalowy <arci1910@wp.pl>
2019-09-30 22:40:51 +0200

    fix missing native libraries

--- src/main/java/com/stalowy/ocrplapp/OcrSpringApplication.java
+++ src/main/java/com/stalowy/ocrplapp/OcrSpringApplication.java
@@ -8,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OcrSpringApplication {
 
     public static void main(String[] args) {
+        //System.setProperty("jna.library.path", "F:\\JAVA\\tess4j\\win32-x86-64");
+        //System.setProperty("java.library.path", "F:\\JAVA\\tess4j\\win32-x86-64");
+
+        System.setProperty("jna.library.path", "src/main/resources/lib/win32-x86-64");
+        System.setProperty("java.library.path", "src/main/resources/lib/win32-x86-64");
+
         SpringApplication.run(OcrSpringApplication.class, args);
         BasicConfigurator.configure();
   }
