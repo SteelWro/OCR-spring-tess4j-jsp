b3e1113b33fd457def6fb5f6636bf379d50ec675
SteelWro <cwiklinski.artur@wp.pl>
2019-10-01 10:06:24 +0200

    heroku fix heroku fix jar push

--- src/main/java/com/stalowy/ocrplapp/OcrSpringApplication.java
+++ src/main/java/com/stalowy/ocrplapp/OcrSpringApplication.java
@@ -8,13 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OcrSpringApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(OcrSpringApplication.class, args);
         //System.setProperty("jna.library.path", "F:\\JAVA\\tess4j\\win32-x86-64");
         //System.setProperty("java.library.path", "F:\\JAVA\\tess4j\\win32-x86-64");
-
         System.setProperty("jna.library.path", "src/main/resources/lib/tess4j/win32-x86-64");
         System.setProperty("java.library.path", "src/main/resources/lib/tess4j/win32-x86-64");
-
-        SpringApplication.run(OcrSpringApplication.class, args);
         BasicConfigurator.configure();
   }
 
