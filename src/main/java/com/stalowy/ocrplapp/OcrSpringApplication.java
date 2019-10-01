94909caa3a522354e5257177af794ff16762c319
SteelWro <cwiklinski.artur@wp.pl>
2019-10-01 14:55:06 +0200

    fixed libraries and deploy app on server

--- src/main/java/com/stalowy/ocrplapp/OcrSpringApplication.java
+++ src/main/java/com/stalowy/ocrplapp/OcrSpringApplication.java
@@ -8,9 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OcrSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OcrSpringApplication.class, args);
         System.setProperty("jna.library.path", "src/main/resources/lib/tess4j/win32-x86-64");
         System.setProperty("java.library.path", "src/main/resources/lib/tess4j/win32-x86-64");
+        SpringApplication.run(OcrSpringApplication.class, args);
         BasicConfigurator.configure();
   }
 
